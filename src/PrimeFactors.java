d625654cef6630548e6c660c5ec907dafa694019
Jernik <Luke.Miller.DRSS@gmail.com>
2015-03-26 23:53:37 -0400

    Changed PrimeFactors to pass test

--- src/PrimeFactors.java
+++ src/PrimeFactors.java
@@ -1,9 +1,10 @@
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class PrimeFactors {
 
 	public static List<Integer> generate(int l){
-		return null;
+		return new ArrayList<Integer>();
 		}
 }
