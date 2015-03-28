09bfe6e3800c8873fa47e118347deb420e332255
Jernik <Luke.Miller.DRSS@gmail.com>
2015-03-27 21:31:45 -0400

    So Im gonna read all the way through instructions

---
+++ src/PrimeGeneratorTest.java
@@ -0,0 +1,53 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+//FIX ALL THIS
+@RunWith(Parameterized.class)
+public class PrimeGeneratorTest{
+	   private Integer inputNumber;
+	   private List<Integer> expectedResult;
+	   private PrimeFactors primeFactors;
+
+	   @Before
+	   public void initialize() {
+	      primeFactors = new PrimeFactors();
+	   }
+
+	   // Each parameter should be placed as an argument here
+	   // Every time runner triggers, it will pass the arguments
+	   // from parameters we defined in primeNumbers() method
+	   public PrimeGeneratorTest(Integer inputNumber, 
+	      List<Integer> expectedResult) {
+	      this.inputNumber = inputNumber;
+	      this.expectedResult = expectedResult;
+	   }
+
+	   @Parameterized.Parameters
+	   public static Collection primeNumbers() {
+	      return Arrays.asList(new Object[][] {
+	    	 {1, new ArrayList()},
+	         { 2, Arrays.asList(2)},
+	         { 3, Arrays.asList(3) },
+	         { 4, Arrays.asList(2,2) },
+	         { 6, Arrays.asList(2,3) },
+	      });
+	   }
+
+	   // This test will run 4 times since we have 5 parameters defined
+	   @Test
+	   public void testPrimeNumberChecker() {
+	      System.out.println("Parameterized Number is : " + inputNumber);
+	      assertEquals(expectedResult, 
+	      primeFactors.generate(inputNumber));
+	   }
+
+}
--- src/PrimeTests.java
+++ src/PrimeTests.java
@@ -2,52 +2,41 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 
-@RunWith(Parameterized.class)
-public class PrimeTests{
-	   private Integer inputNumber;
-	   private List<Integer> expectedResult;
-	   private PrimeFactors primeFactors;
+public class PrimeTests {
 
-	   @Before
-	   public void initialize() {
-	      primeFactors = new PrimeFactors();
-	   }
-
-	   // Each parameter should be placed as an argument here
-	   // Every time runner triggers, it will pass the arguments
-	   // from parameters we defined in primeNumbers() method
-	   public PrimeTests(Integer inputNumber, 
-	      List<Integer> expectedResult) {
-	      this.inputNumber = inputNumber;
-	      this.expectedResult = expectedResult;
-	   }
-
-	   @Parameterized.Parameters
-	   public static Collection primeNumbers() {
-	      return Arrays.asList(new Object[][] {
-	    	 {1, new ArrayList()},
-	         { 2, Arrays.asList(2)},
-	         { 3, Arrays.asList(3) },
-	         { 4, Arrays.asList(2,2) },
-	         { 6, Arrays.asList(2,3) },
-	      });
-	   }
-
-	   // This test will run 4 times since we have 5 parameters defined
-	   @Test
-	   public void testPrimeNumberChecker() {
-	      System.out.println("Parameterized Number is : " + inputNumber);
-	      assertEquals(expectedResult, 
-	      primeFactors.generate(inputNumber));
-	   }
-
+	
+	@Test
+	public void testOne(){
+		assertEquals(list(), PrimeFactors.generate(1));
+	}
+	
+	@Test
+	public void testTwo(){
+		ArrayList<Integer> a=new ArrayList<Integer>();
+		a.add(2);
+		assertEquals(a, PrimeFactors.generate(2));
+	}
+	
+	@Test
+	public void testThree(){
+		assertEquals(Arrays.asList(3),PrimeFactors.generate(3));
+		
+	}
+	
+	@Test
+	public void testFour(){
+		assertEquals(Arrays.asList(2,2),PrimeFactors.generate(4));
+	}
+	
+	private List<Integer> list() {
+		// TODO Auto-generated method stub
+		return Arrays.asList();
+	}
 }
